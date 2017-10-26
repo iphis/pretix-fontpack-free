d2110f267b36837ca461f55df2d44e2e6f1f9d12
Tobias Knipping <knipping@tk-schulsoftware.de>
2017-10-26 11:39:24 +0200

    add Inconsolata-Font

--- pretix_fontpackfree/__init__.py
+++ pretix_fontpackfree/__init__.py
@@ -10,7 +10,7 @@ class PluginApp(AppConfig):
         author = 'Raphael Michel'
         description = 'Pack of free fonts for pretix\' ticket editor'
         visible = False
-        version = '1.0.3'
+        version = '1.0.4'
 
     def ready(self):
         from . import signals  # NOQA
